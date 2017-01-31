f916abb7bdaa1531f3bb3947f1874d2057d62aba
Mick Cooney <michael.cooney@barnett-waddingham.co.uk>
2017-01-31 16:16:22 +0000

    Removed the page breaks and tidied up some of the code

---
+++ sdpsm_intro/render_worksheets.R
@@ -0,0 +1,8 @@
+library(knitr)
+
+
+knit2pdf("sdpsm_worksheet_shell.Rnw")
+knit2pdf("sdpsm_worksheet_git.Rnw")
+knit2pdf("sdpsm_worksheet_tidyverse.Rnw")
+knit2pdf("sdpsm_worksheet_probpyr.Rnw")
+knit2pdf("sdpsm_worksheet_modeling.Rnw")
